265ef656be0e034cf1c955e749cb9f10e382173c
Jeff Lam <lmh.jeff@gmail.com>
2022-10-31 00:34:06 +0800

    fixed skills component

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -12,7 +12,7 @@ const Skill = ({ directionLeft, skill }: IconProps) => {
         <div className="group relative flex cursor-pointer">
             <motion.img
                 initial={{
-                    x: directionLeft ? -200 : 200,
+                    x: directionLeft ? -100 : 100,
                     opacity: 0,
                 }}
                 transition={{ duration: 1 }}
@@ -21,9 +21,9 @@ const Skill = ({ directionLeft, skill }: IconProps) => {
                     x: 0,
                 }}
                 src={urlFor(skill?.image).url()}
-                className="rounded-full border bg-[rgb(36,36,36)] border-gray-500 object-contain w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+                className="rounded-full border bg-[rgb(36,36,36)] border-gray-500 object-contain w-16 h-16 sm:w-24 sm:h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
             />
-            <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
+            <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-16 w-16 sm:w-24 sm:h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
                 <div className="flex items-center justify-center h-full">
                     <p className="text-3xl font-bold text-black opacity-100">{skill.progress}%</p>
                 </div>
@@ -33,6 +33,17 @@ const Skill = ({ directionLeft, skill }: IconProps) => {
 }
 
 export default Skill
+
+
+
+
+
+
+
+
+
+
+
 
 
 
