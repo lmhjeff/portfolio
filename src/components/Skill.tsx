f9c1aba0365106f15c1743e35bb3c85e0a7b5df0
Jeff Lam <lmh.jeff@gmail.com>
2022-10-31 23:22:58 +0800

    fixed skills issue

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -21,9 +21,9 @@ const Skill = ({ directionLeft, skill }: IconProps) => {
                     x: 0,
                 }}
                 src={urlFor(skill?.image).url()}
-                className="rounded-full border bg-[rgb(36,36,36)] border-gray-500 object-contain w-16 h-16 sm:w-24 sm:h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+                className="rounded-full border bg-[rgb(36,36,36)] border-gray-500 object-contain w-16 h-16 sm:w-16 sm:h-16 md:w-16 md:h-16 xl:w-20 xl:h-20 filter group-hover:grayscale transition duration-300 ease-in-out"
             />
-            <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-16 w-16 sm:w-24 sm:h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
+            <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-16 w-16 sm:w-16 sm:h-16 md:w-16 md:h-16 xl:w-20 xl:h-20 rounded-full z-0">
                 <div className="flex items-center justify-center h-full">
                     <p className="text-3xl font-bold text-black opacity-100">{skill.progress}%</p>
                 </div>
@@ -33,6 +33,15 @@ const Skill = ({ directionLeft, skill }: IconProps) => {
 }
 
 export default Skill
+
+
+
+
+
+
+
+
+
 
 
 
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -13,12 +13,12 @@ const Skills = ({ skills }: Props) => {
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
             className="min-h-screen relative flex flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 justify-center xl:space-y-0 mx-auto items-center">
-            <h3 className="uppercase absolute top-12 md:top-24 tracking-[20px] text-gray-500 text-2xl">Skills</h3>
-            <h3 className="absolute top-28 md:top-36 uppercase tracking-[3px] text-gray-500 text-sm">
+            <h3 className="uppercase absolute top-12 md:top-16 tracking-[20px] text-gray-500 text-2xl">Skills</h3>
+            <h3 className="absolute top-28 md:top-32 uppercase tracking-[3px] text-gray-500 text-sm">
                 Hover over a skill for current profieciency
             </h3>
 
-            <div className="grid grid-cols-4 gap-5">
+            <div className="grid grid-cols-4 gap-5 xl:pt-20">
                 {skills?.slice(0, skills.length / 2).map((skill, i) => (
                     <Skill key={skill._id} skill={skill} />
                 ))}
@@ -31,6 +31,12 @@ const Skills = ({ skills }: Props) => {
 }
 
 export default Skills
+
+
+
+
+
+
 
 
 
